0d464981998162276c370fcffab6fdbaf1d7e1f9
@elayaraj31 <elayaraj2004@gmail.com>
2025-04-09 11:18:19 +0530

    done

---
+++ Laptop.java
@@ -0,0 +1,12 @@
+public class Laapto {
+ public static void maind(String[] args)
+{
+Laptop asur = new laptop();
+asur.fature(8,"1TB"10000);
+Laptop hp = new Laptop();
+hp.fature(12,"500gb",12000,10);
+}
+public void fature(int ram,String ssd,int prise)
+{
+}
+}
