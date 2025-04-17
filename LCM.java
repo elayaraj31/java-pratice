99438b79d4a367fe667ee58be962ec4bad519a02
elaya <elayaraj2004@gmail.com>
2025-04-17 22:19:54 +0530

    LCM

---
+++ LCM.java
@@ -0,0 +1,17 @@
+public class LCM
+{
+public static void main(String[] args)
+{
+    int no=1;
+    int no2=30;
+        while(no<=no2){
+    if(no%2==0&&no%3==0)
+{
+System.out.println("CM"+no);
+//break;
+}
+no=no+1;
+}
+}   
+}
+
