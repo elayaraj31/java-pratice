dd5b99d0b2b94d6b895dfd2c956985e6acb35607
elaya <elayaraj2004@gmail.com>
2025-04-21 21:25:58 +0530

    for4

---
+++ For3.java
@@ -0,0 +1,33 @@
+public class For3
+{
+public static void main(String[] args)
+{
+for(int no=1;no<=5;++no,no++)
+{
+System.out.println("hi"); 
+System.out.println("hello"); 
+}
+}
+}
+/*
+output;
+hi
+hello
+hi
+hello
+hi
+hello
+
+2)hi
+hello
+hi
+hello
+hi
+hello
+hi
+hello
+hi
+hello
+
+*/
+
