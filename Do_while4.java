c954907536ff47938b52914432c162c406356c07
elaya <elayaraj2004@gmail.com>
2025-04-21 20:10:44 +0530

    do_while pro4

---
+++ Do_while4.java
@@ -0,0 +1,20 @@
+public class Do_while4
+{
+public static void main(String[] args)
+{
+int no=1;
+    while (no<=5)
+{
+       ++no; no++;
+    System.out.println(no);
+}
+}
+}
+
+OUTPUT:
+1
+2
+3
+4
+5
+
