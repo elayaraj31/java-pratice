a62a4899deade75975436b354e4286a2aee2af7b
elaya <elayaraj2004@gmail.com>
2025-04-21 20:46:09 +0530

    do_while5

---
+++ Do_while5.java
@@ -0,0 +1,18 @@
+public class Do_while5
+{
+public static void main(String[] args)
+{
+int no=1;
+    while (no<=5)
+{
+       no++; ++no;
+    System.out.println(no);
+}
+}
+}
+
+OUTPUT:
+3
+5
+7
+
