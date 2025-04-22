c2e27da731c1929cc9160fce33d27021aa8efb52
elaya <elayaraj2004@gmail.com>
2025-04-22 12:04:55 +0530

    static method

---
+++ Test2.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+public class Test2 {
+	public static void main(String[] args)
+	{
+		Test2 cc = new Test2();
+	int a =	Test2.info();
+	System.out.println(a);
+	}
+	public static int info()
+	{
+		int i=10;
+		int a=i+i;
+	return a;	
+	}
+
+}
+
