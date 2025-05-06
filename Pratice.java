83be702ca1c7e28ef1969a8ef1125102aa1dd2dc
elaya <elayaraj2004@gmail.com>
2025-05-06 20:55:50 +0530

    done

---
+++ Pratice.java
@@ -0,0 +1,18 @@
+package project;
+
+public class Pratice {
+
+	public static void main(String[] args) {
+
+		for(int i=1;i<=5;i=i+1)
+		{
+			for(int j=i;j<=5;j++) 
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
