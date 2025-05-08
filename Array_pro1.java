5482e4fc2b757597dff9dda5b55c2bbe37744919
elaya <elayaraj2004@gmail.com>
2025-05-08 18:49:39 +0530

    array

---
+++ Array_pro1.java
@@ -0,0 +1,12 @@
+public class Array_pro1 {
+
+	public static void main(String[] args)
+	{
+		int[][] marks = {{90, 78, 76}, {90, 78, 36}, {77, 78, 90}};
+		
+	System.out.println(marks[0][0]+" "+marks[0][1]+" "+marks[0][2]);	
+	System.out.println(marks[1][0]+" "+marks[1][1]+" "+marks[1][2]);
+	System.out.println(marks[2][0]+" "+marks[2][1]+" "+marks[2][2]);
+}
+}
+
