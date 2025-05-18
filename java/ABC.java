91be231987c8e7dc345ae3a1d1ac8d20f123d1da
elayaraj31 <elayaraj2004@gmail.com>
2025-05-18 21:29:10 +0530

    java1

--- java/ABC.java
+++
@@ -1,9 +0,0 @@
-public class ABC
-{
-int i=10;
-public static void main(String[] arge)
-{
- System.out.println("ABC");
-
-}
-}
--- java/Home.java
+++
@@ -1,7 +0,0 @@
-public class Home
-{
-public static void main(String[] arge)
-{
- System.out.println("welocme to java");
-}
-}
