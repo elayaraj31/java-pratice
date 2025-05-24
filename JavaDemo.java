70671d834d00e74282cb6881a2eaeffb1d19cc98
elaya <elayaraj2004@gmail.com>
2025-05-24 21:39:16 +0530

    javademo

---
+++ JavaDemo.java
@@ -0,0 +1,15 @@
+public class JavaDemo
+{
+public static void main(String []args){
+            JavaDemo java = new JavaDemo();
+            java.dis();
+
+ }
+public void dis()
+{
+   int i=10;
+    int j=20;
+  int c=i+j;
+    System.out.println(c);
+}
+}
