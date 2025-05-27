2d3a8aa649da66f327b5c35ae076c39070015e60
elaya <elayaraj2004@gmail.com>
2025-05-27 11:52:38 +0530

    power num

---
+++ Power.java
@@ -0,0 +1,19 @@
+	public class Power {
+	
+	public static void main(String[] args) {
+	
+		final String name="sugu";
+		Power ss = new Power();
+		ss.add();
+		System.out.println(name);
+	}
+	public void add() {
+		 int age=23;
+			Power cc = new Power();
+			System.out.println(age);
+
+			cc.add();
+		
+	}
+}
+		
