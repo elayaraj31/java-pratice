15c061d908d559e1bbe4b26ca25d41ecb7ecf7db
elaya <elayaraj2004@gmail.com>
2025-05-31 20:47:14 +0530

    spring

---
+++ SpringWithRectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithRectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithRectApplication.class, args);
+	}
+
+}
