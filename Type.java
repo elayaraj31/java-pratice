7aea077fc080937550f43e36381c3e62e0c5114c
elaya <elayaraj2004@gmail.com>
2025-08-08 18:45:26 +0530

    sample

---
+++ Type.java
@@ -0,0 +1,9 @@
+public class Type {
+    public static void main(String[] args) {
+        short a = 1000; // short range: -32768 to 32767
+        long b;
+        b = a;
+        System.out.print(b);
+    }
+}
+
