a1cb825538745cc32acf88540aba9492f697f1f2
elaya <elayaraj2004@gmail.com>
2025-08-28 15:29:42 +0530

    array to strimg

---
+++ Revers_String.java
@@ -0,0 +1,28 @@
+package String_program;
+
+import java.util.Arrays;
+
+public class Revers_String {
+	
+	public static void main(String[] args) {
+		
+		//1) Normal String Revers 
+		
+		/*String Origenal = "ELAYARAJ";
+		String Copy = "";
+		for(int i = Origenal.length()-1;i>=0;i--) {
+			Copy += Origenal.charAt(i);	
+		}
+		System.err.println(Copy);*/
+		
+		
+		/* 2) USING INBUILD METHOD
+		String Origenal = "ELAYARAJ";
+		String Copy = new StringBuilder(Origenal).reverse().toString();
+		 System.out.println(Copy);*/
+		
+		     
+
+	}
+
+}
